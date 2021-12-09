f7ffdb9a9894cd06ff9dc2c088a9947b78197fd6
Jason Leaver <jason.leaver.developer@gmail.com>
2021-12-09 06:05:36 +0000

    .

--- src/views/react-tafgen/index.tsx
+++ src/views/react-tafgen/index.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useReducer, useRef } from 'react';
 import Box from '@mui/material/Box';
 import ContentEditable from 'react-contenteditable';
 import { Command, useC2 } from './controller/c2';
-import './parser';
+import { parser, sampleTaf } from './parser';
+import JSONTree from 'react-json-tree';
 // import * as peggy from 'peggy';
 // const ICAO = 'KADW';
 // let TAF = `
@@ -18,15 +19,19 @@ import './parser';
 // } catch (e) {
 // 	console.log(e);
 // }
-
+// const testTAF = `\
+// TAF KADW 070100Z 0701/0807 01015G25KT 9999 BKN020 QNH2902INS
+// BECMG 0704/0705 01015G25KT 9999 BKN020 QNH2902INS
+// BECMG 0704/0705 01015G25KT 9999 BKN020 QNH2902INS TX13/0421Z TN03/0508Z\
+// `;
 //style
 const monokai = {
-	background: 'rgb(30,31,28)',
-	foreground: 'rgb(48,40,34)',
-	active: 'rgb(62,61,50)',
-	red: 'rgb(249,38,114)',
-	blue: 'rgb(102,217,239)',
-	green: 'rgb(116,226,40)',
+    background: 'rgb(30,31,28)',
+    foreground: 'rgb(48,40,34)',
+    active: 'rgb(62,61,50)',
+    red: 'rgb(249,38,114)',
+    blue: 'rgb(102,217,239)',
+    green: 'rgb(116,226,40)',
 };
 
 // let tafExample = `
@@ -38,76 +43,88 @@ const monokai = {
 // `;
 
 export default function TafGen() {
-	return (
-		<Command>
-			<div style={{ height: '100vh', width: '100%', backgroundColor: monokai.background, color: monokai.red }}>
-				<Box
-					component='form'
-					sx={{
-						'& .MuiTextField-root': { m: 1, width: '25ch' },
-					}}
-					noValidate
-					autoComplete='off'>
-					<Pad>
-						<h2>TAF-LINT 0.1</h2>
-					</Pad>
-					<TafLinter />
-				</Box>
-			</div>
-		</Command>
-	);
+    return (
+        <Command>
+            <div style={{ height: '100vh', width: '100%', backgroundColor: monokai.background, color: monokai.red }}>
+                <Box
+                    component='form'
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete='off'>
+                    <Pad>
+                        <h2>TAF-LINT 0.1</h2>
+                    </Pad>
+                    <TafLinter />
+                </Box>
+            </div>
+        </Command>
+    );
 }
 
 function TafLinter() {
-	const {
-		state: { rows },
-	} = useC2();
-	const [parserValue, setParserValue] = useState<any>(null);
-	const style: React.CSSProperties = {
-		fontFamily: 'monospace',
-		fontSize: '20px',
-		// padding: 2,
-		accentColor: 'blue',
-		textTransform: 'uppercase',
-		height: 200,
-		color: monokai.green,
-		backgroundColor: monokai.foreground,
-		border: '1px solid #888',
-	};
-	// useEffect(() => {
-	//     try {
+    const {
+        state: { rows },
+    } = useC2();
 
-	//         const v = parser.parse('TAF KADW 12015G25KT 5000 SHRA SCT020 QNH2970INS', {});
-	//         setParserValue(v)
+    const style: React.CSSProperties = {
+        fontFamily: 'monospace',
+        fontSize: '20px',
+        // padding: 2,
+        accentColor: 'blue',
+        textTransform: 'uppercase',
+        height: 200,
+        color: monokai.green,
+        backgroundColor: monokai.foreground,
+        border: '1px solid #888',
+    };
+    const [passValue, setPass] = useState<any>(null);
+    const [failValue, setFail] = useState<any>(null);
 
-	//     } catch (e) {
-	//         setParserValue(e)
-	//         // throw new Error(`THERE WAS AN ERROR${e}`)
-	//     }
-	// }, [])
-	return (
-		<>
-			{JSON.stringify(parserValue)}
-			<Pad>
-				<div style={style}>
-					<Pad>{rows.map((row: JSX.Element) => row)}</Pad>
-				</div>
-			</Pad>
-		</>
-	);
+    useEffect(() => {
+        try {
+
+            const v = parser.parse(sampleTaf, {});
+            setPass(v)
+            setFail(null)
+            // setParserValue(v)
+
+        } catch (e) {
+            setPass(null)
+            setFail(e)
+            // throw new Error(`THERE WAS AN ERROR${e}`)
+        }
+    }, [])
+    return (
+        <>
+            {!!passValue ? passValue.map(v => <JSONTree hideRoot data={JSON.stringify(v)} />) : null}
+            {!!failValue ? <Fail data={failValue.stack} /> : null}
+
+            <Pad>
+                <div style={style}>
+                    <Pad>{rows.map((row: JSX.Element) => row)}</Pad>
+                </div>
+            </Pad>
+        </>
+    );
 }
-
+function Fail({ data }) {
+    return <div>
+        <JSONTree data={data} />
+    </div>
+}
 function reducer({ ...oldState }, { type, ...event }) {
-	switch (type) {
-		case 'blur':
-			return { ...oldState, backgroundColor: 'inherit' };
-		case 'focus':
-			return { ...oldState, backgroundColor: monokai.active };
+    switch (type) {
+        case 'blur':
+            return { ...oldState, backgroundColor: 'inherit' };
+        case 'focus':
+            return { ...oldState, backgroundColor: monokai.active };
 
-		default:
-			// console.log(type);
-			return oldState;
-	}
+        default:
+            // console.log(type);
+            return oldState;
+    }
 }
 
 /**
@@ -131,67 +148,67 @@ TAF, changing only the TAF header and the erroneous elements (e.g., if a TAF
 originally starting at 1600Z is corrected at 1615Z, all forecast g
  */
 export function HEAD() {
-	const { state } = useC2();
-	console.log(state);
-	return <LINE immutableText={`${state.icao}`} />;
+    const { state } = useC2();
+    console.log(state);
+    return <LINE immutableText={`${state.icao}`} />;
 }
 
 export function TEMPO() {
-	return <LINE immutableText={'TEMPO'} />;
+    return <LINE immutableText={'TEMPO'} />;
 }
 export function BECMG() {
-	return <LINE immutableText={'BECMG'} />;
+    return <LINE immutableText={'BECMG'} />;
 }
 const { localStorage } = window;
 
 function LINE({ immutableText }) {
-	const {
-		onEnter,
-		// state: { parser },
-	} = useC2();
-	const ref: React.MutableRefObject<any> = useRef();
-	const [{ backgroundColor }, dispatch] = useReducer(reducer, {
-		backgroundColor: 'inherit',
-		// innerHTML: '',
-	});
-	const [html, setHtml] = useState('');
-	useEffect(() => {
-		const cat = localStorage.getItem('TAF-LAB');
-		if (!!cat) setHtml(cat);
-	}, []);
-	const handleChange = (evt) => {
-		const { value } = evt.target;
-		localStorage.setItem('TAF-LAB', value);
+    const {
+        onEnter,
+        // state: { parser },
+    } = useC2();
+    const ref: React.MutableRefObject<any> = useRef();
+    const [{ backgroundColor }, dispatch] = useReducer(reducer, {
+        backgroundColor: 'inherit',
+        // innerHTML: '',
+    });
+    const [html, setHtml] = useState('');
+    useEffect(() => {
+        const cat = localStorage.getItem('TAF-LAB');
+        if (!!cat) setHtml(cat);
+    }, []);
+    const handleChange = (evt) => {
+        const { value } = evt.target;
+        localStorage.setItem('TAF-LAB', value);
 
-		try {
-			// const result = parser.parse(value);
-		} catch (e) {
-			console.log(e);
-		}
-		// console.log(result);
-		setHtml(value);
-	};
-	useEffect(() => {
-		if (!!ref.current) {
-			ref.current.addEventListener('keydown', (e) => {
-				if (e.key === 'Enter') {
-					e.preventDefault();
-					onEnter(e);
-				}
-			});
-		}
-	}, [onEnter]);
-	return (
-		<div onBlur={dispatch} onFocus={dispatch}>
-			{immutableText}
-			<ContentEditable
-				style={{ backgroundColor, width: '100', display: 'inline-block' }}
-				innerRef={ref}
-				html={html} // innerHTML of the editable div
-				onChange={handleChange} // handle innerHTML change
-			/>
-		</div>
-	);
+        try {
+            // const result = parser.parse(value);
+        } catch (e) {
+            console.log(e);
+        }
+        // console.log(result);
+        setHtml(value);
+    };
+    useEffect(() => {
+        if (!!ref.current) {
+            ref.current.addEventListener('keydown', (e) => {
+                if (e.key === 'Enter') {
+                    e.preventDefault();
+                    onEnter(e);
+                }
+            });
+        }
+    }, [onEnter]);
+    return (
+        <div onBlur={dispatch} onFocus={dispatch}>
+            {immutableText}
+            <ContentEditable
+                style={{ backgroundColor, width: '100', display: 'inline-block' }}
+                innerRef={ref}
+                html={html} // innerHTML of the editable div
+                onChange={handleChange} // handle innerHTML change
+            />
+        </div>
+    );
 }
 
 const Pad = ({ ...props }) => <div style={{ padding: 10 }} {...props} />;
--- src/views/react-tafgen/parser.ts
+++ src/views/react-tafgen/parser.ts
@@ -1,143 +1,274 @@
 import * as peggy from 'peggy';
 
 function makeParser(Icao: string, baseTime: string, isConusLocation: boolean = true) {
-	console.log(baseTime);
-	const reportableVisibility = () =>
-		////////////////////////////////
-		`"${[
-			//
-			...['0000', '0100', '0200', '0300', '0400', '0600', '0700', '0800', '0900'], //0000-0999
-			...['1000', '1100', '1200', '1200', '1400', '1500', '1600', '1700', '1800'], //1000-1999
-			...['2000', '2200', '2400', '2600', '2800', '3000', '3200', '3400', '3600', '3700'], //2000-3999
-			...['4000', '4400', '4500', '4700', `${isConusLocation ? '5000' : '4800'}`],
-			...['6000', '7000', '8000', '9000', '9999'],
-		].join('" / "')}"`;
+    console.log(baseTime);
+    const reportableVisibility = () =>
+        ////////////////////////////////
+        `"${[
+            //
+            ...['0000', '0100', '0200', '0300', '0400', '0600', '0700', '0800', '0900'], //0000-0999
+            ...['1000', '1100', '1200', '1200', '1400', '1500', '1600', '1700', '1800'], //1000-1999
+            ...['2000', '2200', '2400', '2600', '2800', '3000', '3200', '3400', '3600', '3700'], //2000-3999
+            ...['4000', '4400', '4500', '4700', `${isConusLocation ? '5000' : '4800'}`],
+            ...['6000', '7000', '8000', '9000', '9999'],
+        ].join('" / "')}"`;
+    // ''.slice(1)
+    return peggy.generate(`
+//** ------------------------------------------------------------
+{{
+    function makeInteger(o) {
+      return parseInt(o.join(""), 10);
+    }
+  }}
+  
+  {
+    if (options.multiplier) {
+      input = "(" + input + ")*(" + options.multiplier + ")";
+    }
+  }
+  
+RUN = MessageHeading IntermediateLines+  TemperatureGroup 
 
-	return peggy.generate(`
-    //** ------------------------------------------------------------
+//** ------------------------------------------------------------
+__ "Single White Space Indicator" = 
+    [ \\t\\n\\r] 
+
+_NL "New Line Indicator" = 
+    [\\n]
+//** ------------------------------------------------------------
+
+IntermediateLines =
+    BECMG_Group / TEMPO_Group
+
+
+//** ------------------------------------------------------------
+
+MessageHeading =
+  "TAF ${Icao}" TAFTimes Line
+
+
     
-    RUN = START BECMG+ END
-    
-    //** ------------------------------------------------------------
-    __ "Single White Space" = [ \\t\\n\\r]
-    _NL "New Line Indicator" = [\\n]
-    //** ------------------------------------------------------------
-    //** ------------------------------------------------------------
-    START =
-        "TAF ${Icao}" BaseTime Line 
-        
-    BECMG = 
-        (_NL "BECMG" BECMG_TIME Line)
-    
-    BECMG_TIME = __ DD HH "/" DD HH
-    
-    
-    
-    TEMPO = 
-        (_NL "TEMPO")
-    
-    END = 
-        MaxTemp MinTemp
-    //** ------------------------------------------------------------
-    BaseTime "Issue & Valid Time Range DDHHMMZ DDHH/DDHH" =
-        IssueTime ValidRange
-    
-    
-    IssueTime = __ DD HH MM "Z"
-    
-    ValidRange = __ DD HH "/" DD HH
-    
-    
-    DD 'Day'= [0,1,2,3][0-9]
-    HH 'Hour' = [0,1,2][0-9]
-    MM 'Minute' =
-        [0,1,2,3,4,5,6][0-9]
-    
-    
-    //** ------------------------------------------------------------
-    
-    Line = 
-         Winds Visibility CurrentWeather SkyCondition Altsg 
-    
-    
-    //** ----------------------| WX |----------------------
-    CurrentWeather "Current Weather"= 
-        wx:WX  {return {currentWx:wx.map(v=>!!v?v.join(""):"")}}
-    
-    WX = 
-        (__ Mods "RA" )? (__ Mods "SHRA")? (__ "BR" )? 
-    
-    Mods = 
-        "" / "-" / "+"
-    
-    //** ----------------------| WINDS |----------------------
-    Winds =
-        dir:WindDir spd:WindSpeed {
-            return {
-                dir:Number(dir.join("")),
-                spd:spd.join("")
-        }}
-    
-    WindDir =
-        __ [0,1,2,3][0-9][0]
-    
-    WindSpeed =
-        [0-9][0-9]"KT" / [0-9][0-9]"G"[0-9][0-9]"KT"
-    //** ----------------------| VISIBILITY |----------------------
-        
-    Visibility = 
-        __ ReportableVisibility
-    
-    ReportableVisibility =
-        ${reportableVisibility()} 
-    
-    //** ----------------------| SKY_CONDITION |----------------------
-    SkyCondition =
-        __ ClourCover CloudBase
-    ClourCover = 
-        "SCT" / "FEW" / "BKN" / "OVC" 
-    
-    CloudBase = 
-        [0,1,2,3][0-9][0-9]
-    
-    
-    //** ----------------------| ALTSG |----------------------
-    Altsg =
-        __ "QNH" ("29"/ "30") [0-9][0-9] "INS"
-    
-    
-    
-    //** ----------------------| END OF TAF |----------------------
-    MaxTemp =
-        (__ "TX" temp "/" time "Z")
-    
-    MinTemp =
-        (__ "TN" temp "/" time "Z")
-    
-    temp = 
-        [0-9][0-9]
-    
-    time =  
-        [0-9][0-9][0-9][0-9]
-    
-    
-    
-    
-    `);
+
+//** ------------------------|  BECMG   |------------------------------------    
+BECMG_Group "BECMG Group" = 
+     _NL "BECMG" becmgGroup:( BECMG_TimeRange Line ) {
+         return { becmgGroup }
+        }
+
+
+BECMG_TimeRange "BECMG Group Valid Time" =
+    __ startTime:( DD HH ) "/" endTime: ( DD HH ) {
+
+    return {startTime,endTime}
 }
 
-const testTAF = `\
-TAF KADW 070100Z 0701/0807 01015G25KT 9999 BKN020 QNH2902INS
-BECMG 0704/0705 01015G25KT 9999 BKN020 QNH2902INS
-BECMG 0704/0705 01015G25KT 9999 BKN020 QNH2902INS TX13/0421Z TN03/0508Z\
+
+//** ------------------------------------------------------------
+
+TEMPO_Group "Tempo Group"= 
+    _NL "TEMPO" ( TEMPO_TimeRange TEMPO_Line )
+
+
+TEMPO_Line "Tempo Line" = 
+    __ line :(  WX )
+
+
+TEMPO_TimeRange "TEMPO Group Valid Time" =
+    __ startTime:( DD HH ) "/" endTime: ( DD HH ) {
+    return {startTime,endTime}
+}
+
+
+
+
+
+//** ------------------------------------------------------------
+TAFTimes "Issue & Valid Time Period DDHHMMZ DDHH/DDHH" =
+    issued:TAF_IssueTime validPeriod:TAF_ValidPeriod { 
+        return {...issued, validPeriod }
+    }
+
+TAF_IssueTime "TAF Issue Time" = 
+    __ dateTime:( DD HH MM ) "Z" {
+        const issued = new Date(Date.UTC(2021, 1, ...dateTime))
+        return { issued }
+    }
+
+TAF_ValidPeriod "TAF Valid Range" = 
+     __ startTime:( DD HH ) "/" endTime: ( DD HH ) {
+        const start = new Date(Date.UTC(2021,1, ...startTime))
+        const stop = new Date(Date.UTC(2021,1, ...endTime))//.getHours()
+        return { start, stop }
+     }
+
+
+DD 'Day'= 
+    dd:([0,1,2][0-9] / [3][0,1])  {return dd.join("")}
+
+HH 'Hour' = 
+    hh:([0,1][0-9] / [2][1,2,3,4]) {return hh.join("") }
+
+MM 'Minute' =
+    mm:([0,1,2,3,4,5,6][0-9]) { return mm.join("") }
+
+
+//** ------------------------------------------------------------
+
+Line = 
+    WindGroup VisibilityGroup CurrentWeather SkyCondition Altsg 
+
+
+//** ----------------------| WX |----------------------
+CurrentWeather "Current Weather"= 
+    wx:WX  {return {currentWx:wx.map(v=>!!v?v.join(""):"")}}
+
+WX = 
+    (__ Mods "RA" )? (__ Mods "SHRA")? (__ "BR" )? 
+
+Mods = 
+    "" / "-" / "+"
+
+//** ----------------------| WINDS |----------------------
+// direction:WindDirection speed:( SustainedWinds / GustingWinds )  {
+WindGroup "Wind Group (dddffGfmfmKT)"=
+    __ 
+    direction:(ddd:([0,1,2][0-9][0] / [3][0,1,2][0] / "VRB") { 
+        if(ddd==="VRB") return ddd
+
+        return ddd.join("")
+    }) 
+    speed:( SustainedWinds / GustingWinds )  {
+
+        if(direction ==="VRB" && Number(speed)>6){
+            throw Error('\
+            When wind speed will be more than 6 knots, do not use VRB for ddd \
+            unless the situation involves air-mass thunderstorm activity during which \
+            forecasting a prevailing wind direction with confidence is not possible. When it is \
+            possible to forecast the peak gust direction, but not the prevailing direction, \
+            encode the wind group as VRBffGfmfmKT and append the probable peak gust \
+            direction to remarks (e.g., GST DRCTN 250).\
+            ')
+        }
+
+         return {wind:[direction,...speed]}
+     }
+
+
+WindDirection =
+    __ ddd:([0,1,2][0-9][0] / [3][0,1,2][0] / "VRB") { 
+        return ddd
+
+    }
+
+
+    
+
+SustainedWinds =
+    windSpeed:(ValidWinds) "KT" {return [windSpeed]}
+
+GustingWinds "Wind Gusts" =
+    ff:ValidWinds "G" fm:ValidWinds "KT" {
+        const ws = Number(ff)
+        const wg = Number(fm)
+        if(ws>=wg) throw Error("Forecast Gust Should be greater than Forecast Windspeeds")
+        return [ff, fm]
+    }
+
+// VairableWinds =
+// vrb:("VRB" ValidWinds){return [vrb]}
+
+
+ValidWinds =
+     wind:([0-9][0-9]) {return wind.join("")}
+
+
+
+//** ----------------------| VISIBILITY |----------------------
+    
+VisibilityGroup "Visibility Group (VVVV)" = 
+    (__ ReportableVisibility) {return }
+
+ReportableVisibility =
+    vis:(${reportableVisibility()}) ${submit('Number(vis)')}
+
+//** ----------------------| SKY_CONDITION |----------------------
+
+
+SkyCondition =
+     sc:(SkyCon+ / SKC ) { return sc.join("") }
+
+SkyCon = __(ClourCover CloudBase)
+
+SKC = ( __ "SKC" )
+
+ClourCover = 
+    ("SCT" / "FEW" / "BKN" / "OVC" )
+
+CloudBase = 
+    cb:([0,1,2,3][0-9][0-9]) {return cb.join("")}
+
+
+//** ----------------------| ALTSG |----------------------
+Altsg =
+   ( __ "QNH" ("29"/ "30") [0-9][0-9] "INS")
+
+
+DateHourUTC = 
+endTime:(DD HH) {
+        return new Date(Date.UTC(2021,1, ...endTime))//.getHours()
+    }
+//** -------| Forecast Maximum and Minimum Temperature groups |--------------------
+
+
+TemperatureGroup "1.3.5.1. Forecast Maximum and Minimum Temperature groups (T(X)(N)[M]TFTF/YYGFGFZ)"= 
+    max: MaxTemp min: MinTemp {
+        const [Tx, Dx, Hx] = max
+        const [Tn, Dn, Hn] = min
+
+
+
+        return {TX:max,TN:min}
+    }
+
+MaxTemp =
+    __ "TX" tx:Temperature "/" vt:DateHourUTC "Z" {
+        // const [dd]
+        const dt = Date(2024,)
+        return [tx, ...vt]
+    }
+
+MinTemp =
+     __ "TN" tx:Temperature "/" vt:DateHourUTC "Z" {
+         return [tx,...vt]
+     }
+
+Temperature = 
+    tt:("M"?[0-9][0-9]) { 
+        const [minus, ...temp] = tt
+        if(!!minus) tt = ['-',...temp]
+        return Number(tt.join(""))
+     }
+    
+
+    
+    
+`);
+}
+
+function submit(x) {
+
+    return `{return ${x}}`
+}
+
+
+
+
+
+export const sampleTaf = `\
+TAF KADW 280100Z 0701/0807 01010KT 9999 SKC QNH2902INS
+BECMG 0716/0705 01015G17KT 9999 BKN020 BKN020 QNH2902INS
+BECMG 0704/0705 VRB06KT 9999 BKN020 QNH2902INS TX13/0421Z TNM03/0508Z\
 `;
-const parser = makeParser('KADW', '070100Z');
-try {
-	const pVal = parser.parse(testTAF);
+export const parser = makeParser('KADW', '070100Z');
 
-	console.log('PARSER PASSED');
-	pVal.forEach((e) => console.log(e));
-} catch (e) {
-	console.log('PARSER FAILED');
-	console.log(e);
-}
+
